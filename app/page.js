76b919144e1f4774e0acbc46627e03a20cd84327
ddooo00 <1995thdud@naver.com>
2024-01-13 23:19:29 +0900

    react basic -3 #2

--- app/page.js
+++ app/page.js
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <div></div>;
+  let name = "lee";
+  return (
+    <div>
+      <div>
+        <h4 style={{ textAlign: "center", marginTop: "150px" }}>애플후레시</h4>
+        <p style={{ textAlign: "center" }}> by dooo00</p>
+      </div>
+    </div>
+  );
 }

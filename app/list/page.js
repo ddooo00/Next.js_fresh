16124552b309d1ed8b8550048ce07bac3206a844
ddooo00 <1995thdud@naver.com>
2024-01-16 22:42:34 +0900

    feat / list page.js clean code #2

--- app/list/page.js
+++ app/list/page.js
@@ -1,13 +1,16 @@
 export default function List() {
+  let 상품 = ["Tomatoes", "Pasta", "Coconut"];
   return (
     <div>
       <h4 style={{ textAlign: "center", marginTop: "150px" }}>상품 목록</h4>
-      <div className="food">
-        <h4>상품1 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품2 $45</h4>
-      </div>
+      {상품.map((a, i) => {
+        return (
+          <div className="food">
+            <h4>{a} $40</h4>
+            {/* <h4>{상품[i]}</h4> */}
+          </div>
+        );
+      })}
     </div>
   );
 }
